d74e802c944653a545ee8ac5175f56fe530f0736
Cody Marsh <cody_marsh@fastmail.us>
2023-01-04 21:00:49 -0700

    025

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,2 +1,5 @@
 const d = new Date();
 document.getElementById("currentyear").innerHTML = d.getFullYear();
+
+let lastdatetimemodified = document.lastModified;
+document.getElementById("lastModified").innerHTML = lastdatetimemodified;
