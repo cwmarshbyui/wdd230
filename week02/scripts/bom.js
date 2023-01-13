bc7802b4028b77e86876b3874fa838013d8ab7f5
Cody Marsh <cody_marsh@fastmail.us>
2023-01-12 20:09:27 -0700

    1/12

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -3,22 +3,21 @@ const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
 
-button.addEventListener('click', function () {
+button.addEventListener('click', () => {
     if (input.value != '') {
         const myItem = input.value;
+        // li.textContent = input.value;
+        li.textContent = myItem;
+        deleteButton.textContent = '❌';
+        li.append(deleteButton);
+        li.append('testing 123 cody ');
+        list.appendChild(li);
         input.value = '';
     }
 });
 
 const li = document.createElement('li');
 const deleteButton = document.createElement('button');
-
-// li.textContent = input.value;
-li.textContent = myItem;
-deleteButton.textContent = '❌';
-li.append(deleteButton);
-li.append('testing 123 cody ');
-list.append(li);
 
 deleteButton.addEventListener('click', function () {
     list.removeChild(li);
