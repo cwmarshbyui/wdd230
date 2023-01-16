5c44605885bf4f75936b1468d02c924d1b6c6b4e
Cody Marsh <cody_marsh@fastmail.us>
2023-01-16 09:00:48 -0700

    Week 3 LA

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -6,14 +6,14 @@ const list = document.querySelector('#list');
 button.addEventListener('click', () => {
     if (input.value != '') {
         const myItem = input.value;
-        // li.textContent = input.value;
+        li.textContent = input.value;
         li.textContent = myItem;
         deleteButton.textContent = '❌';
         li.append(deleteButton);
-        // li.append('testing 123 cody ');
         list.append(li);
         console.log(list)
         input.value = '';
+        input.focus();
     }
 });
 
@@ -25,5 +25,9 @@ deleteButton.addEventListener('click', function () {
     input.focus();
 });
 
+li.textContent = myItem;
+deleteButton.textContent = '❌';
+li.append(deleteButton);
+list.append(li);
 input.focus();
 input.value = '';
---
+++ week03/scripts/getDates.js
@@ -0,0 +1,5 @@
+d = new Date();
+document.getElementById("currentyear").innerHTML = d.getFullYear();
+
+lastdatetimemodified = document.lastModified;
+document.getElementById("lastModified").innerHTML = lastdatetimemodified;
