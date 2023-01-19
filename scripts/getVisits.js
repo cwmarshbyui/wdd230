b627faec940719d059dd08c1ea196839e271155e
Cody Marsh <cody_marsh@fastmail.us>
2023-01-18 21:14:46 -0700

    1/18

--- scripts/getVisits.js
+++ scripts/getVisits.js
@@ -1,1 +1,22 @@
 /* This is where I'll put the get visits js code. */
+// initialize display elements
+const visitsDisplay = document.querySelector(".visits");
+
+// get the stored value in localStorage
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+
+// determine if this is the first visit or display the number of visits.
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit!`;
+}
+
+// increment the number of visits.
+numVisits++;
+
+// store the new number of visits value
+localStorage.setItem("visits-ls", numVisits);
+
+// You can view the localStorage data using the Applications panel in the browsers's DevTools.
+
