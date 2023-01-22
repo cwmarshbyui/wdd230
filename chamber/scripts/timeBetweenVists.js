f55d85a2ddd17b5330c430292ac3c2b7bae43a60
Cody Marsh <cody_marsh@fastmail.us>
2023-01-21 19:00:16 -0700

    1/21 7pm

--- chamber/scripts/timeBetweenVists.js
+++ chamber/scripts/timeBetweenVists.js
@@ -1,9 +1,10 @@
 /* This is where I'll put the get visits js code. */
 // initialize display elements
 const timeBetweenVisitsDisplay = document.querySelector(".discovertimebetweenvisits");
+const minutesBetweenVisitsDisplay = document.querySelector(".discoverminutesbetweenvisits");
 
 // get the stored value in localStorage
-let lastVisitTimeString = Number(window.localStorage.getItem("visitTime-ls"));
+let lastVisitTimeString = window.localStorage.getItem("visitTime-ls");
 let lastVisitTime = new Date(lastVisitTimeString);
 
 // get the current visit time
@@ -15,10 +16,12 @@ if (lastVisitTime !== null) {
 
     // calculate difference between visit times.
     let datediff = currentVisitTime - lastVisitTime;
-    daysDiff = Math.floor(datediff/1000/60/60/24).toString();
+    let daysDiff = Math.floor(datediff / 1000 / 60 / 60 / 24).toString();
+    let minutesDiff = Math.floor(datediff / 1000 / 60).toString();
 
     // Set the value in html output.
-    timeBetweenVisitsDisplay.textContent = daysDiff.concat(" days betweem visits.");
+    timeBetweenVisitsDisplay.textContent = daysDiff.concat(" days between visits.");
+    minutesBetweenVisitsDisplay.textContent = minutesDiff.concat(" minutes between visits.");
 } else {
     timeBetweenVisitsDisplay.textContent = `This the first visit!`;
 }
