abc9d49ef0535df6b0b2e3b8f2a01ed6c5ee2ee4
Cody Marsh <cody_marsh@fastmail.us>
2023-01-31 21:16:38 -0700

    1/31/2023

--- week05/scripts/weather.js
+++ week05/scripts/weather.js
@@ -3,4 +3,34 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather';
+// const url = 'https://api.openweathermap.org/data/2.5/weather';
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Trier&appid=07d271ffbafb4a7722ddc51b2fb2fa27&units=imperial';
+
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            // console.log(data); // testing only
+            displayResults(data); // uncomment when ready
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+apiFetch();
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    // const iconsrc = "https://openweathermap.org/img/w/04n.png";
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+}
+
