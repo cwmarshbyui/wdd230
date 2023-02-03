0bd0b70166366f11110b75d12976ffdc2311ff12
Cody Marsh <cody_marsh@fastmail.us>
2023-02-02 17:40:54 -0700

    2/2/2023

--- ztest/scripts/ztest.js
+++ ztest/scripts/ztest.js
@@ -1,18 +1,15 @@
-// select HTML elements in the document
-const currentTemp = document.querySelector('#current-temp');
-const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('#weathercaption');
 
-// const url = 'https://api.openweathermap.org/data/2.5/weather';
-const url = 'https://api.openweathermap.org/data/2.5/weather?q=Buckeye&appid=07d271ffbafb4a7722ddc51b2fb2fa27&units=imperial';
-
+const arrayTest = document.querySelector('#arraytest');
+const arrayTesTheWeek = document.querySelector('#arraytesttheweek');
+const url = '../activities3.json';
 
 async function apiFetch() {
     try {
         const response = await fetch(url);
         if (response.ok) {
             const data = await response.json();
-            // console.log(data); // testing only
+            console.log(data.length);
+
             displayResults(data); // uncomment when ready
         } else {
             throw Error(await response.text());
@@ -25,11 +22,8 @@ async function apiFetch() {
 apiFetch();
 
 function displayResults(data) {
-    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
-    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-    let desc = data.weather[0].description;
-    weatherIcon.setAttribute('src', iconsrc);
-    weatherIcon.setAttribute('alt', desc);
-    captionDesc.textContent = `${desc}`;
-}
+    arrayTest.innerHTML = data.length;
+    arrayTesTheWeek.innerHTML = data[1].length;
+    alert(data[0].length);
 
+}
