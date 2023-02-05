c61c03cdcac26bc96ef6d96d1844f983ae2cedd8
Cody Marsh <cody_marsh@fastmail.us>
2023-02-04 23:00:39 -0700

    2/4

--- scripts/getVisits.js
+++ scripts/getVisits.js
@@ -3,14 +3,15 @@
 const visitsDisplay = document.querySelector(".visits");
 
 // get the stored value in localStorage
-let numVisits = Number(window.localStorage.getItem("visits-ls"));
+let numVisits = Number(window.localStorage.getItem("visits-ls")) || 0;
 
 // determine if this is the first visit or display the number of visits.
 if (numVisits !== 0) {
+	console.log("visitsDisplay.textContent = numVisits; " + numVisits);
 	visitsDisplay.textContent = numVisits;
 } else {
 	visitsDisplay.textContent = `This is your first visit!`;
-}
+};
 
 // increment the number of visits.
 numVisits++;
